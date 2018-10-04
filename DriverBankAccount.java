ce985538dd0dd06d6a1c4b565ee31ee3d044da98
stefansor <ssorobay00@stuy.edu>
2018-10-04 11:01:38 -0400

    yep

--- DriverBankAccount.java
+++ DriverBankAccount.java
@@ -1,23 +1,37 @@
-public class DriverBankAccount {
+public class DriverBankAccount{
   public static void main(String[]args){
-    BankAccount a = new BankAccount(400, 12345678, "tighten up by the drells");
-    System.out.println("Account: " + a.getAccountID() + "\t" + "Balance: " + a.getBalance()
-    + "Password: " + a.getPassword());
+    BankAccount b1 = new BankAccount(1000.0, 1000001, "abc123");
+    BankAccount b2 = new BankAccount(1234.0, 1000231, "abcasdg23");
 
-    a.setPassword("tighten up by Archie Bell and the Drells");
-    System.out.println("Account: " + a.getAccountID() + "\t" + "Balance: " + a.getBalance()
-    + "Password: " + a.getPassword());
+    System.out.println("Account b1:");
+    System.out.println(b1);
+    System.out.println("Account b2:");
+    System.out.println(b2);
+    System.out.println();
 
-    a.deposit(200);
-    System.out.println("Account: " + a.getAccountID() + "\t" + "Balance: " + a.getBalance()
-    + "Password: " + a.getPassword());
 
-    a.withdraw(300);
-    System.out.println("Account: " + a.getAccountID() + "\t" + "Balance: " + a.getBalance()
-    + "Password: " + a.getPassword());
+    double cashAmount = 300.0;
+    while(cashAmount < 3000){
+      System.out.println("Attempt to move "+cashAmount+" from the b1 to b2 account:");
+      if(b1.withdraw(cashAmount) ){
+        if(b2.deposit(cashAmount)){
+          System.out.println("Success");
+        }else{
+          //This should never happen.
+          //Error message provided to make sure that is the case
+          System.out.println(cashAmount + " withdrawn from" +
+                             b1 + "Failed to add to "+b2);
+        }
+      }else{
+        System.out.println("Failed to withdraw "+cashAmount+" from "+b1);
+      }
 
-    a.withdraw(400);
-    System.out.println("Account: " + a.getAccountID() + "\t" + "Balance: " + a.getBalance()
-    + "Password: " + a.getPassword());
+      System.out.println("Account b1:");
+      System.out.println(b1);
+      System.out.println("Account b2:");
+      System.out.println(b2);
+      System.out.println();
+      cashAmount *= 2;
+    }
   }
 }
